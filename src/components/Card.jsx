3d4a8d6328c01688481fb0ab0900d505bc733af9
Mayra Alzate <nadja.minni@gmail.com>
2026-03-04 11:07:43 -0500

    Creacion componentes -Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+    return(
+        <article>
+            <section></section>
+            <section>
+                <h4><b>TITULO</b></h4>
+            </section>
+        </article>
+    )
+}
+
+export default Card
